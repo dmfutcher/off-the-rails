2e0af49c3ef5731caf7111cc1054f479de2cfae2
Elliot Iddon <elliot@citrusbytes.net>
2021-10-03 12:55:02 +0000

    Update function to resolve redirects
    Uses axios to resolve the eventual redirected URL.
    Throw away the query string to reduce tracking. This will eventually
    cause problems since some sites uses the query string to reference
    resources (e.g.  YouTube).

--- functions/derail.js
+++ functions/derail.js
@@ -1,3 +1,20 @@
+const axios = require('axios');
+const {parse, format} = require('url');
+
+const stripQuery = (url) => {
+  const parsed = parse(url, true);
+  parsed.search = undefined;
+  parsed.query = undefined;
+  return format(parsed);
+}
+
+
+const fetchRedirect = async (url) => {
+  const stripped = stripQuery(url);
+  const response = await axios.get(stripped);
+  return stripQuery(response.request.res.responseUrl);
+}
+
 exports.handler = function (event, context, callback) {
   let url;
   try {
@@ -9,11 +26,22 @@ exports.handler = function (event, context, callback) {
     return { statusCode: 400, body: "invalid/missing JSON body, url must be provided" };
   }
 
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({url}),
+  fetchRedirect(url).then(response => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({url: response}),
+    });
+  }).catch(e => {
+    console.error(e);
+    callback(null, {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({error: 'Failed to resolve redirection'}),
+    });
   });
 };
